87ccb200246cb1c0e014b2a5b836bb26d973fcaf
Alex Bazaly <alex.bazaly@drumncode.com>
2023-09-16 16:16:41 +0300

    installer debugyes

--- installer.sh
+++ installer.sh
@@ -4,44 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 APP_PATH="/opt/ipmonitor"
 DOCKER_REQ_VERSION="24"
 
-
-
-
-
-
-
-	check_folder() {
-    if [[ -z $1 ]]; then
-        echo "Error: Folder path not provided."
-        return 1
-    fi
-
-    local folder_path="$1"
-
-    if [[ -d $folder_path && -n $(ls -A "$folder_path") ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-
-
-
-#echo "We need your sudo session to proceed. Thanks a lot"
-#if [ $EUID != 0 ]; then
-#    sudo "$0" "$@"
-#    exit $?
-#fi
-
-
 echo "Update apt cache and install tools:"
-#sudo apt update
-#sudo apt install make git -y;
+sudo apt update
+sudo apt install make git -y;
 echo ""
 echo "Preparing working dir"
-echo "Checking application folder if it exists, just pull git reporsitory to update version, if not, create dir and git clone installer repo"
-echo "..."
+echo "Checking application folder if it exists, if not, create dir and git clone installer repo"
 
 if [[ -d $APP_PATH && -n $(ls -A "$APP_PATH") ]]; then
  echo "Project folder exists and is not empty."
@@ -60,17 +28,11 @@ else
   echo "Cloned installer files from public repository into $APP_PATH"
   cp .env.install .env
   echo ""
-  echo "Before we sart, please, edit .environment file to correct your data from preset values to your own:"
+  echo "Before we start, please, edit .environment file to correct your data from preset values to your own:"
   echo ""
   nano .env
 
 fi
-
-#echo ""
-#echo "===================Docker section ======================="
-#
-# Check if Docker is already installed
-
 
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Installing Docker..."
@@ -90,9 +52,6 @@ if ! command -v docker &> /dev/null; then
     docker_version=$(sudo docker --version  2>&1)
     echo "Docker version: $docker_version"
 fi
-
-
-
 
 if command -v docker &> /dev/null; then
     docker_version=$(docker --version | awk '{print $3}' | cut -d ',' -f1)
@@ -114,44 +73,3 @@ if command -v docker &> /dev/null; then
         sudo -E make dc-first-install-app
 fi
 
-
-#  echo "Checking if .env file exists. If it doesn't exist, copy from template"
-#    if [ ! -f ${APP_PATH}/.env ]; then
-#      cp ${APP_PATH}/.env.install ${APP_PATH}/.env || exit
-#      echo "Template .env copied to .env"
-#    fi
-
-#echo ""
-#echo "===================Docker section ======================="
-#
-#echo "Checking if docker installed, if installed, what version is installed"
-#check_docker_version
-##if [ -z "$(check_docker_version)" ]; then
-##  echo "docker isn't installed";
-##  echo ""
-##  dockerinstalled=0
-##fi
-#if [ -z "$(check_docker_version)" ]; then
-#echo "installing docker:"
-##from official docs here: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
-## Add Docker's official GPG key:
-#sudo apt-get install ca-certificates curl gnupg
-#sudo install -m 0755 -d /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#sudo chmod a+r /etc/apt/keyrings/docker.gpg
-#
-## Add the repository to Apt sources:
-#echo \
-#  "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  \"$(. /etc/os-release && echo "$VERSION_CODENAME")\" stable" | \
-#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-#echo "Docker version installed: $(check_docker_version)"
-#fi
-#
-#
-#cd ${APP_PATH} || exit
-#cat ~/pass.txt | docker login https://index.docker.io/v1/ --username alexbazdnc --password-stdin
-#
-#make dc-init-app
